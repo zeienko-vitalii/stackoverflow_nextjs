515e93e97a2cff3b2ffbcf393d5d98919fe5cf4a
Vitaly Zeenko <dev.zeenko@gmail.com>
2024-04-16 18:49:00 +0200

    Add RightSidebar component and update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "@/components/shared/navbar/Navbar";
 import LeftSidebar from "@/components/shared/navbar/LeftSidebar";
+import RightSidebar from "@/components/shared/navbar/RightSidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
+        <RightSidebar />
       </div>
       Toaster
     </main>
